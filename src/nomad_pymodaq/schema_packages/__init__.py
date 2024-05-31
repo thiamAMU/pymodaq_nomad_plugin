cccd85f3cc0e8b17728e33d36d1dcb8736e727c9
Astou Thiam <thiamastou053@gmail.com>
2024-05-31 15:21:08 +0200

    Added sintering classes from yaml schema

--- src/nomad_pymodaq/schema_packages/__init__.py
+++ src/nomad_pymodaq/schema_packages/__init__.py
@@ -15,3 +15,17 @@ mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
     description='Schema package defined using the new plugin mechanism.',
 )
+
+
+class SinteringEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_pymodaq.schema_packages.sintering import m_package
+
+        return m_package
+
+
+sintering = SinteringEntryPoint(
+    name='Sintering',
+    description='Schema package for describing a sintering process.',
+)
